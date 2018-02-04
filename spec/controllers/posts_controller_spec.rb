4557cb957352269bcb28b6edcc6946f352dd7c8b
Drew Maring <drew.maring@docker.com>
2018-02-04 12:49:37 -0800

    completed checkpoint 19

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -68,11 +68,62 @@ RSpec.describe PostsController, type: :controller do
   end
 
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, params: { id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+    #1 we expect the edit view to render when a post is edited
+    it "renders the #edit view" do
+      get :edit, params: { id: my_post.id }
+      expect(response).to(render_template(:edit))
+    end
+    #2 we test that edit assigns the correct post to be updated to @post
+    it "assigns post to be updated to @post" do
+      get :edit, params: { id: my_post.id }
 
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end
+
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+      #3, we test that @post was updated with the title and body passed to update. We also test that @post's id was not changed.
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      #4, we expect to be redirected to the posts show view after the update.
+      put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the post" do
+      delete(:destroy, params: { id: my_post.id })
+      #6, we search the database for a post with an id equal to my_post.id. This returns an Array. We assign the size of the array to count, and we expect count to equal zero. This test asserts that the database won't have a matching post after  destroy is called.
+      count = Post.where({id: my_post.id}).size
+      expect(count).to(eq(0))
+    end
+
+    it "redirects to posts index" do
+      delete :destroy, params: { id: my_post.id }
+      #7, we expect to be redirected to the posts index view after a post has been deleted.
+      expect(response).to(redirect_to(posts_path))
+    end
+  end
 end
