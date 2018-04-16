73d5b787d48a5021a7b608e3b901f42139edf967
Drew Maring <andrew.maring@gmail.com>
2018-04-15 19:20:46 -0700

    complete checkpoint 21

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # method that
+  def form_group_tag(errors, &block)
+    css_class = 'form-group'
+    # add ' has error' to CSS string being passed if errors occur
+    css_class << ' has-error' if errors.any?
+    # content_tag is a Rails helper method that builds HTML and CSS
+    content_tag :div, capture(&block), class: css_class
+  end
 end
