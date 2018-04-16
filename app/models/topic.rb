0b065bf1e0ca401d9c5bd8ad6e451a1bce2dd191
Drew Maring <andrew.maring@gmail.com>
2018-04-15 20:10:26 -0700

    complete assignment 21

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
+
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 10 }, presence: true
+  validates :public, presence: true
+
 end
